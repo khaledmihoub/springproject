d649fb72635e2f5385acf0a1282eb3cabc4bf751
Khaled Mihoub <49345013+khaledmihoub@users.noreply.github.com>
2023-02-14 15:59:35 +0100

    repositories

---
+++ src/main/java/com/esprit/springproject/repositories/SkieurRepository.java
@@ -0,0 +1,11 @@
+package com.esprit.springproject.repositories;
+
+
+import com.esprit.springproject.entities.Skieur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkieurRepository extends JpaRepository<Skieur, Integer> {
+
+
+
+}
